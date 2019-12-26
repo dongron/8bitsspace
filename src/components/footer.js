b8dc586674e113ececa89a927396012ba228218f
Dominik <dongron@wp.pl>
2019-12-26 13:52:56 +0700

    Initial app commit

---
+++ src/components/footer.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import {Container, Row, Col} from 'reactstrap'
+import { FaFacebookSquare, FaInstagram } from 'react-icons/fa';
+import Link from './link';
+
+let SocialLink = ({Icon}) => (
+  <Link to="/" className="mr-2">
+    <Icon size={30}/>
+  </Link>
+)
+
+let FooterLink = ({to, children}) => (
+  <li>
+    <Link to={to}>
+      {children}
+    </Link>
+  </li>
+)
+
+export default () => (
+  <footer className="py-5 bg-light">
+    <Container>
+      <Row>
+        <Col>
+          <h5>Features</h5>
+          <ul>
+            <FooterLink to="/">Item</FooterLink>
+          </ul>
+        </Col>
+        <Col>
+          <h5>Resources</h5>
+          <ul>
+            <FooterLink to="/">Item</FooterLink>
+          </ul>
+        </Col>
+        <Col>
+          <h5>Company</h5>
+          <ul>
+            <FooterLink to="/">Item</FooterLink>
+          </ul>
+        </Col>
+        <Col>
+          <h5>Contact Us</h5>
+          <SocialLink Icon={FaFacebookSquare}/>
+          <SocialLink Icon={FaInstagram}/>
+        </Col>
+      </Row>
+    </Container>
+  </footer>
+)
